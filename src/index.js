ce7d65cb41942e629f0803a0a37372f628781b3e
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-31 14:32:42 +0200

    add merge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> af7fe18155467931f25766ad7e8e76aeb758b4ea
->>>>>>> 3600ab3fa862677f4b0f2eeb042ef0e2b5f3e815
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/App.scss";
@@ -12,27 +5,10 @@ import Collapsible from "./components/Collapsible-title";
 import Design from "./components/Design";
 import Fill from "./components/Fill";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import Share from "./components/Share";
 import Section1 from "./components/Section1";
 import Header from "./components/Header";
-=======
-<<<<<<< HEAD
 import Main from "./components/Main";
-=======
-import Section1 from "./components/Section1";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.scss';
-import Header from './components/Header';
-import Collapsible from './components/Collapsible-title';
-import Design from './components/Design';
-import Fill from './components/Fill';
-import Footer from './components/Footer';
->>>>>>> 9cbb6422904bc978a1a3105ccdb0222deba254e2
->>>>>>> af7fe18155467931f25766ad7e8e76aeb758b4ea
->>>>>>> 3600ab3fa862677f4b0f2eeb042ef0e2b5f3e815
 
 ReactDOM.render(
   <React.StrictMode>
@@ -41,15 +17,9 @@ ReactDOM.render(
     <Design />
     <Fill />
     <Footer />
-<<<<<<< HEAD
     <Share />
-=======
-<<<<<<< HEAD
     <Main />
-=======
->>>>>>> 3600ab3fa862677f4b0f2eeb042ef0e2b5f3e815
     <Section1 />
->>>>>>> af7fe18155467931f25766ad7e8e76aeb758b4ea
   </React.StrictMode>,
   document.getElementById("root")
 );
