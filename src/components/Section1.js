a6c50c355af51ad281232461dcb6d579deb2e7cb
adrmenendez <adrmenendez@gmail.com>
2021-05-31 16:16:52 +0200

    fix responsive

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,47 +1,67 @@
-import React, { Component } from "react";
-import "../styles/App.scss";
-import photo from "../images/ilustraciones.gif";
+import React, { Component } from 'react';
+import '../styles/App.scss';
+import photo from '../images/ilustraciones.gif';
 
 class Section1 extends Component {
   render() {
     return (
-      <section className="container1">
-        <button className="button_reset" type="reset">
-          <i className="fa fa-trash" aria-hidden="true"></i>reset
+      <section className='container1'>
+        <button className='button_reset' type='reset'>
+          <i className='fa fa-trash' aria-hidden='true'></i>reset
         </button>
-        <div className="card">
-          <div className="card_header">
-            <div className="rectangle"></div>
-            <div className="name_job">
-              <div className="name">Mileva Marić</div>
-              <div className="job">Matemática</div>
+        <div className='card'>
+          <div className='card_header'>
+            <div className='rectangle'></div>
+            <div className='name_job'>
+              <div className='name'>Mileva Marić</div>
+              <div className='job'>Matemática</div>
             </div>
           </div>
           <img
-            className="card_image js__profile-preview"
+            className='card_image js__profile-preview'
             src={photo}
-            alt=""
+            alt=''
           ></img>
 
-          <div className="logos">
-            <div className="icon js-icon1 opacity">
-              <a href="tel:" className="link1 js-phone" target="_blank">
-                <i className="fas fa-mobile-alt iconcolor"></i>
+          <div className='logos'>
+            <div className='icon js-icon1 opacity'>
+              <a
+                href='tel:'
+                className='link1 js-phone'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <i className='fas fa-mobile-alt iconcolor'></i>
               </a>
             </div>
-            <div className="icon js-icon2 opacity">
-              <a href="mailto:" className="link1 js-email" target="_blank">
-                <i className="far fa-envelope iconcolor"></i>
+            <div className='icon js-icon2 opacity'>
+              <a
+                href='mailto:'
+                className='link1 js-email'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <i className='far fa-envelope iconcolor'></i>
               </a>
             </div>
-            <div className="icon js-icon3 opacity">
-              <a href="" className="link1 js-linkedin" target="_blank">
-                <i className="fab fa-linkedin-in iconcolor"></i>
+            <div className='icon js-icon3 opacity'>
+              <a
+                href=''
+                className='link1 js-linkedin'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <i className='fab fa-linkedin-in iconcolor'></i>
               </a>
             </div>
-            <div className="icon js-icon4 opacity">
-              <a href="" className="link1 js-github" target="_blank">
-                <i className="fab fa-github-alt iconcolor"></i>
+            <div className='icon js-icon4 opacity'>
+              <a
+                href=''
+                className='link1 js-github'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <i className='fab fa-github-alt iconcolor'></i>
               </a>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/App.scss";
-import Collapsible from "./components/Collapsible-title";
-import Design from "./components/Design";
-import Fill from "./components/Fill";
-import Footer from "./components/Footer";
-import Share from "./components/Share";
-import Section1 from "./components/Section1";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/App.scss';
+import Collapsible from './components/Collapsible-title';
+import Design from './components/Design';
+import Fill from './components/Fill';
+import Footer from './components/Footer';
+import Share from './components/Share';
+// import Section1 from './components/Section1';
+import Header from './components/Header';
+import Card from './components/Main-card';
+// import Main from './components/Main';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Collapsible />
+    <Card />
     <Design />
     <Fill />
     <Footer />
     <Share />
-    <Main />
-    <Section1 />
+    {/* <Main /> */}
+    {/* <Section1 /> */}
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
