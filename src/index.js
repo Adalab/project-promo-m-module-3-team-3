b9b24685370415eb28643e9e02df7a01affe63f4
Laura733 <lali733@gmail.com>
2021-05-31 19:56:12 +0200

    changes main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> af7fe18155467931f25766ad7e8e76aeb758b4ea
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/App.scss";
@@ -9,21 +5,10 @@ import Collapsible from "./components/Collapsible-title";
 import Design from "./components/Design";
 import Fill from "./components/Fill";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import Main from "./components/Main";
-=======
+
 import Section1 from "./components/Section1";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.scss';
-import Header from './components/Header';
-import Collapsible from './components/Collapsible-title';
-import Design from './components/Design';
-import Fill from './components/Fill';
-import Footer from './components/Footer';
->>>>>>> 9cbb6422904bc978a1a3105ccdb0222deba254e2
->>>>>>> af7fe18155467931f25766ad7e8e76aeb758b4ea
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -32,11 +17,8 @@ ReactDOM.render(
     <Design />
     <Fill />
     <Footer />
-<<<<<<< HEAD
     <Main />
-=======
     <Section1 />
->>>>>>> af7fe18155467931f25766ad7e8e76aeb758b4ea
   </React.StrictMode>,
   document.getElementById("root")
 );
