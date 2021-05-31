b37f388d5594ee31f22f8245c1df07a9cd7b22c1
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-05-31 14:00:42 +0200

    add componets section1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.scss';
-import Collapsible from './components/Collapsible-title';
-import Design from './components/Design';
-import Fill from './components/Fill';
-import Footer from './components/Footer';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/App.scss";
+import Collapsible from "./components/Collapsible-title";
+import Design from "./components/Design";
+import Fill from "./components/Fill";
+import Footer from "./components/Footer";
+import Section1 from "./components/Section1";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Design />
     <Fill />
     <Footer />
+    <Section1 />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
