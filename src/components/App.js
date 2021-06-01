2f9f8c18e63ed5642df686954587fcc31fae1c1d
adrmenendez <adrmenendez@gmail.com>
2021-06-01 15:53:34 +0200

    fix styles and tags card preview

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,44 @@
-import React from "react";
-import "../styles/App.scss";
-import Footer from "./Footer";
-import Header from "./Header";
-import Design from "./Design";
-import Fill from "./Fill";
-import Share from "./Share";
-import CardPreview from "./CardPreview";
-import Title from "./Title";
+import React from 'react';
+import '../styles/App.scss';
+import Footer from './Footer';
+import Header from './Header';
+import Design from './Design';
+import Fill from './Fill';
+import Share from './Share';
+import CardPreview from './CardPreview';
+import Title from './Title';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      <main className="cards_main">
-        <section className="section1">
+      <main className='cards_main'>
+        <section className='cards_main--preview'>
           <CardPreview />
         </section>
-        <section className="section2">
-          <form className="js-form form">
-            <Title
-              nameClass="js-cdesign"
-              title="Diseña"
-              iconClass="fa-object-ungroup"
-              arrowClass="js-arrowD"
-            />
-            <Design />
-            <Title
-              nameClass="js-cfill"
-              title="rellena"
-              iconClass="fa-keyboard"
-              arrowClass="js-arrowF"
-            />
-            <Fill />
-            <Title
-              nameClass="js-cshare"
-              title="comparte"
-              iconClass="fas fa-share-alt"
-              arrowClass="js-arrowS"
-            />
-            <Share />
-          </form>
-        </section>
+        <form className='js-form form cards_main--form'>
+          <Title
+            nameClass='js-cdesign'
+            title='Diseña'
+            iconClass='fa-object-ungroup'
+            arrowClass='js-arrowD'
+          />
+          <Design />
+          <Title
+            nameClass='js-cfill'
+            title='rellena'
+            iconClass='fa-keyboard'
+            arrowClass='js-arrowF'
+          />
+          <Fill />
+          <Title
+            nameClass='js-cshare'
+            title='comparte'
+            iconClass='fas fa-share-alt'
+            arrowClass='js-arrowS'
+          />
+          <Share />
+        </form>
       </main>
       <Footer />
     </div>
