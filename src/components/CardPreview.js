720d032e1c5e59934a8ec6a74ce45e7a7d734168
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-01 13:05:51 +0200

    add title props

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -1,67 +1,67 @@
-import React, { Component } from 'react';
-import '../styles/App.scss';
-import photo from '../images/ilustraciones.gif';
+import React, { Component } from "react";
+import "../styles/App.scss";
+import photo from "../images/ilustraciones.gif";
 
 class CardPreview extends Component {
   render() {
     return (
-      <section className='container1'>
-        <button className='button_reset' type='reset'>
-          <i className='fa fa-trash' aria-hidden='true'></i>reset
+      <section className="container1">
+        <button className="button_reset" type="reset">
+          <i className="fa fa-trash" aria-hidden="true"></i>reset
         </button>
-        <div className='card'>
-          <div className='card_header'>
-            <div className='rectangle'></div>
-            <div className='name_job'>
-              <div className='name'>Mileva Marić</div>
-              <div className='job'>Matemática</div>
+        <div className="card">
+          <div className="card_header">
+            <div className="rectangle"></div>
+            <div className="name_job">
+              <div className="name">Mileva Marić</div>
+              <div className="job">Matemática</div>
             </div>
           </div>
           <img
-            className='card_image js__profile-preview'
+            className="card_image js__profile-preview"
             src={photo}
-            alt=''
+            alt=""
           ></img>
 
-          <div className='logos'>
-            <div className='icon js-icon1 opacity'>
+          <div className="logos">
+            <div className="icon js-icon1 opacity">
               <a
-                href='tel:'
-                className='link1 js-phone'
-                target='_blank'
-                rel='noreferrer'
+                href="tel:"
+                className="link1 js-phone"
+                target="_blank"
+                rel="noreferrer"
               >
-                <i className='fas fa-mobile-alt iconcolor'></i>
+                <i className="fas fa-mobile-alt iconcolor"></i>
               </a>
             </div>
-            <div className='icon js-icon2 opacity'>
+            <div className="icon js-icon2 opacity">
               <a
-                href='mailto:'
-                className='link1 js-email'
-                target='_blank'
-                rel='noreferrer'
+                href="mailto:"
+                className="link1 js-email"
+                target="_blank"
+                rel="noreferrer"
               >
-                <i className='far fa-envelope iconcolor'></i>
+                <i className="far fa-envelope iconcolor"></i>
               </a>
             </div>
-            <div className='icon js-icon3 opacity'>
+            <div className="icon js-icon3 opacity">
               <a
-                href=''
-                className='link1 js-linkedin'
-                target='_blank'
-                rel='noreferrer'
+                href="https"
+                className="link1 js-linkedin"
+                target="_blank"
+                rel="noreferrer"
               >
-                <i className='fab fa-linkedin-in iconcolor'></i>
+                <i className="fab fa-linkedin-in iconcolor"></i>
               </a>
             </div>
-            <div className='icon js-icon4 opacity'>
+            <div className="icon js-icon4 opacity">
               <a
-                href=''
-                className='link1 js-github'
-                target='_blank'
-                rel='noreferrer'
+                href="https"
+                className="link1 js-github"
+                target="_blank"
+                rel="noreferrer"
               >
-                <i className='fab fa-github-alt iconcolor'></i>
+                <i className="fab fa-github-alt iconcolor"></i>
               </a>
             </div>
           </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import '../styles/App.scss';
+import React, { Component } from "react";
+import "../styles/App.scss";
 
 class Title extends Component {
   render() {
     return (
-      <div className='collapsiblebox @@clase'>
-        <div className='collapsible'>
-          <div className='collapsible__icon'>
-            <i className='far @@icon'></i>
+      <div className={`collapsiblebox ${this.props.nameClass}`}>
+        <div className="collapsible">
+          <div className="collapsible__icon">
+            <i className={`far ${this.props.iconClass}`}></i>
           </div>
-          <div className='collapsible__title'>Diseña{/* @@title */}</div>
-          <div className='arrow'>
-            <i className='fas fa-chevron-down collapsible__icon2 @@arrowmove'></i>
+          <div className="collapsible__title">{this.props.title}</div>
+          <div className="arrow">
+            <i
+              className={`fas fa-chevron-down collapsible__icon2 ${this.props.arrowClass}`}
+            ></i>
           </div>
         </div>
-        <div className='line'></div>
+        <div className="line"></div>
       </div>
     );
   }
