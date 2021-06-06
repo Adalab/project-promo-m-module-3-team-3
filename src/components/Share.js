5a2ac25ea35f82d4d0b433636d188deca82cbfa7
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-06 12:54:00 +0200

    add changes to share.scss and share.js

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import '../styles/App.scss';
+import React, { Component } from "react";
+import "../styles/App.scss";
 
 class Share extends Component {
   render() {
     return (
-      <div className='marginform'>
-        <fieldset className='share_form js-share'>
-          <div className='container__share'>
-            <button
-              className='container__share--button js-createcard'
-              type='button'
+      //<div className="marginform">
+      <fieldset className="share__form js-share">
+        <div className="container__share">
+          <button
+            className="container__share__button js-createcard"
+            type="button"
+          >
+            <a
+              className="share__button--link"
+              title="Crear Tarjeta"
+              target="_blank"
             >
-              <a
-                className='share__button--link'
-                title='Crear Tarjeta'
-                target='_blank'
-              >
-                <i className='far fa-address-card'></i>Crear Tarjeta
-              </a>
-            </button>
-            <div className='twitter_button js-response hidden'></div>
-            <button
-              className='container__share--twitter js-twitter hidden'
-              type='button'
+              <i className="far fa-address-card"></i>Crear Tarjeta
+            </a>
+          </button>
+          <div className="twitter_button js-response hidden"> </div>
+          <button
+            className="container__share__twitter js-twitter hidden"
+            type="button"
+          >
+            <a
+              className="share__twitter--link js-linkTwitter"
+              title="Twitter"
+              target="_blank"
             >
-              <a
-                className='share__twitter--link js-linkTwitter'
-                title='Twitter'
-                target='_blank'
-              >
-                <i className='fab fa-twitter'></i>
-                Compartir en Twitter
-              </a>
-            </button>
-          </div>
-        </fieldset>
-      </div>
+              <i className="fab fa-twitter"></i>
+              Compartir en Twitter
+            </a>
+          </button>
+        </div>
+      </fieldset>
+      //</div>
     );
   }
 }
