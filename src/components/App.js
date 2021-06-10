a3502dd7ca3ef4abd9c901f1ed2b73832134d029
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-06-10 20:26:55 +0200

    add kk

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../styles/App.scss";
 import Card from "./Card/Card";
-//import Landing from "./Landing/Landing";
+import Landing from "./Landing/Landing";
+import Link from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Landing /> */}
-      <Card />
+      <Landing>
+        {/* <Card exact path="/" component={App} />
+      </Landing>
+      <Link to="./Card/Card"></Link> */}
     </div>
   );
 }
