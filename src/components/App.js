fbb1a98c02e899154cde425c1b95cfc1a48fb126
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-06-10 21:00:01 +0200

    add react roter

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import '../styles/App.scss';
-import Card from './Card/Card';
-//import Page from "./Landing/Page";
+import React from "react";
+import "../styles/App.scss";
+import Card from "./Card/Card";
+import Page from "./Landing/Page";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      {/* <Page /> */}
-      <Card />
+    <div className="App">
+      <Switch>
+        <Route path="/" component={Page} />
+        <Route path="/card" component={Card} />
+      </Switch>
     </div>
   );
 }
--- src/components/Landing/Page.js
+++ src/components/Landing/Page.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
-import Landing from '../Landing';
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import Landing from "./Landing";
 
 const Page = (props) => {
   return (
