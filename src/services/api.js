6480bd780fbb547257cfb47d1519d62c9bfffe6c
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-13 12:13:55 +0200

    add services folder

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const ENDPOINT = "https://awesome-profile-cards.herokuapp.com/card";
+
+const postDataFetch = () => {
+  return (
+    fetch(ENDPOINT),
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(data),
+    }.then((response) => response.json())
+  );
+};
