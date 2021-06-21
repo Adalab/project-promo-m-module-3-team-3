010844dea46995f461c4e6a1afb75b060951e281
adrmenendez <adrmenendez@gmail.com>
2021-06-21 12:23:21 +0200

    delete line

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles/App.scss';
 import Main from './Card/Main';
 import Landing from './Landing/Landing';
-import Header from './Header';
 import Footer from './Footer';
 import PageNotFound from './PageNotFound';
 import { Route, Switch } from 'react-router-dom';
